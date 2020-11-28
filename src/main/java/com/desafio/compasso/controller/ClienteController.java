b1cc210b6ffdad2eedf43aba6ce8b58eb79c5f75
Maico Camargo <maicocamargo08@hotmail.com>
2020-11-28 18:07:52 -0300

    ajuste nos endpoints do cliente, e add TO DO

--- src/main/java/com/desafio/compasso/controller/ClienteController.java
+++ src/main/java/com/desafio/compasso/controller/ClienteController.java
@@ -4,6 +4,8 @@ import com.desafio.compasso.model.Cliente;
 import com.desafio.compasso.service.ClienteService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/cliente")
 @ResponseBody
@@ -12,7 +14,23 @@ public class ClienteController {
 
     @GetMapping("{id}")
     private Cliente getClienteById(@PathVariable int id){
-        System.out.println("id: "+ id);
         return new ClienteService().getById(id);
     }
+
+    @GetMapping(params = "nome")
+    private Cliente getClienteByNome(@RequestParam("nome") String nome){
+        return new ClienteService().getByNome(nome);
+    }
+
+    @GetMapping( params = "estado")
+    private List<Cliente> getClienteByEstado(@RequestParam("estado") String estado){
+        return new ClienteService().getByEstado(estado);
+    }
+
+    @DeleteMapping("{id}")
+    private String deletarCliente(@PathVariable int id ){
+        return new ClienteService().deletar(id);
+    }
+
+    //todo add request patch para editar o nome do cliente
 }
--- src/main/java/com/desafio/compasso/service/ClienteService.java
+++ src/main/java/com/desafio/compasso/service/ClienteService.java
@@ -1,6 +1,11 @@
 package com.desafio.compasso.service;
 
+import com.desafio.compasso.model.Cidade;
 import com.desafio.compasso.model.Cliente;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ClienteService {
 
@@ -8,4 +13,18 @@ public class ClienteService {
         return new Cliente();
 
     }
+
+    public Cliente getByNome(String nome) {
+        return new Cliente("AngryM4n","Masculino", new Date("01/01/2020"),23, new Cidade());
+    }
+
+    public String deletar(int id) {
+        return "deletado";
+    }
+
+    public List<Cliente> getByEstado(String estado) {
+     return new ArrayList<>();
+    }
+
+    // todo criar metodo para editar nome do usuário
 }
