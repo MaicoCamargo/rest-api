541aca3bfa692cdcb62a71d0d5cf8688b6105b74
Maico Camargo <maicocamargo08@hotmail.com>
2020-11-28 22:26:38 -0300

    add comentarios nos endpoints /cidade

--- src/main/java/com/desafio/compasso/controller/CidadeController.java
+++ src/main/java/com/desafio/compasso/controller/CidadeController.java
@@ -16,11 +16,21 @@ public class CidadeController {
     @Autowired
     CidadeService service;
 
+    /**
+     * buscar cidade por estado
+     * @param estado - estado para buscar no banco
+     * @return - lista de cidade daquele estado
+     */
     @GetMapping(params = "estado")
     private List<Cidade> getByEstado(@RequestParam String estado) {
         return service.findByEstado(estado);
     }
 
+    /**
+     * buscar cidade por nome
+     * @param nome - nome para buscar no banco
+     * @return - lista de cidade com determinado nome
+     */
     @GetMapping(params = "nome")
     private List<Cidade> getByNome(@RequestParam String nome) {
         return service.findByNome(nome);
--- src/main/java/com/desafio/compasso/service/CidadeService.java
+++ src/main/java/com/desafio/compasso/service/CidadeService.java
@@ -1,6 +1,5 @@
 package com.desafio.compasso.service;
 
-import com.desafio.compasso.exception.RecursoNaoEncontrado;
 import com.desafio.compasso.model.Cidade;
 import com.desafio.compasso.repository.CidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +13,22 @@ public class CidadeService {
     @Autowired
     CidadeRepository repository;
 
+    /**
+     * buscar cidade pelo estado
+     * @param estado - sigla de um estado
+     * @return - lista de cidades com este estado
+     */
     public List<Cidade> findByEstado(String estado) {
 
         return repository.findByEstado(estado);
 
     }
 
+    /**
+     * buscar cidade pelo nome
+     * @param nome - nome de uma cidade
+     * @return - lista de cidades com determinado nome
+     */
     public List<Cidade> findByNome(String nome) {
         return repository.findByNome(nome);
     }
